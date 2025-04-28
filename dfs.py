b9977bb060d63cb1ae43c08fd2034f4a783e1b19
zoteva-m@surf.dev <zoteva-m@surf.dev>
2025-04-28 21:53:48 +0300

    Добавлена возможность считать длину пути от вершины a до b

--- dfs.py
+++ dfs.py
@@ -1,11 +1,15 @@
-def dfs(graph, start, visited=None):
+def dfs(graph, start, end, visited=None):
     if visited is None:
-        visited = set()
-    visited.add(start)
+        visited = [start]
+    if start == end:
+        return visited
     for neighbor in graph.get(start, []):
         if neighbor not in visited:
-            dfs(graph, neighbor, visited)
+            new_path = dfs(graph, neighbor, end, visited + [neighbor])
+            if new_path:
+                return new_path
     return visited
 
 graph = {4: [2], 1: [3], 2: [4]}
-print(dfs(graph, 1))
+path = dfs(graph, 2, 4)
+print(len(path) - 1 if path else "Нет пути")
